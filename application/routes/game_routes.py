392620c893edf1b988ad5d13c2933d6b9b66ca9a
SamJSui <samuelsui2002@gmail.com>
2024-05-02 12:15:39 -0400

    feat(game_routes.py): More RESTful API

--- application/routes/game_routes.py
+++ application/routes/game_routes.py
@@ -10,7 +10,7 @@ from flask import Blueprint, jsonify
 game_blueprint = Blueprint('game', __name__, url_prefix='/api')
 game_controller = GameController()
 
-@game_blueprint.route('/patch_notes/<patch_version>', methods=['GET'])
+@game_blueprint.route('/patch/notes/<patch_version>', methods=['GET'])
 def get_patch_notes(patch_version):
     ''' Retrieves a patch notes document from the MongoDB collection.
 
@@ -24,7 +24,7 @@ def get_patch_notes(patch_version):
     response, status_code = game_controller.get_patch_notes(patch_version)
     return jsonify(response), status_code
 
-@game_blueprint.route('/patch_notes/<patch_version>/<champion_name>', methods=['GET'])
+@game_blueprint.route('/patch/notes/<patch_version>/<champion_name>', methods=['GET'])
 def get_patch_notes_champion(patch_version, champion_name):
     ''' Retrieves a patch notes document for a specific champion from the MongoDB collection.
 
@@ -39,7 +39,7 @@ def get_patch_notes_champion(patch_version, champion_name):
     response, status_code = game_controller.get_patch_notes_champion(patch_version, champion_name)
     return jsonify(response), status_code
 
-@game_blueprint.route('/patch_stats/<patch_version>', methods=['GET'])
+@game_blueprint.route('/patch/stats/<patch_version>', methods=['GET'])
 def get_patch_stats(patch_version):
     ''' Retrieves a patch stats document from the MongoDB collection.
 
@@ -53,7 +53,7 @@ def get_patch_stats(patch_version):
     response, status_code = game_controller.get_patch_stats(patch_version)
     return jsonify(response), status_code
 
-@game_blueprint.route('/patch_stats/<patch_version>/<champion_name>', methods=['GET'])
+@game_blueprint.route('/patch/stats/<patch_version>/<champion_name>', methods=['GET'])
 def get_patch_stats_champion(patch_version, champion_name):
     ''' Retrieves a patch stats document for a specific champion from the MongoDB collection.
 
